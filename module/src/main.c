30a5ed49789cd4095cd106a0e35e4982253c3228
Zero <sadegh.nobakhti@gmail.com>
2024-09-08 01:57:50 +0330

    dot

--- module/src/main.c
+++ module/src/main.c
@@ -8,8 +8,10 @@
 #include <linux/if_tun.h>
 #include <linux/ioctl.h>
 #include <linux/fs.h>
-#include "linux/err.h"
-#include "linux/file.h"
+#include <linux/err.h>
+#include <linux/file.h>
+#include <linux/netdevice.h>
+#include <net/sock.h>
 
 #define DRIVER_AUTHOR "Peter Jay Salzman <p@dirac.org>"
 #define DRIVER_DESC   "A sample driver"
@@ -21,18 +23,33 @@ static int __init init_hello_4(void)
     struct file* file = filp_open("/dev/net/tun", O_RDWR, 0);
     printk(KERN_INFO "Opened tun file\n");
 
-    struct ifreq ifr; memset(&ifr, 0, sizeof(ifr));
+    struct ifreq ifr = {0};
     ifr.ifr_flags = IFF_TUN | IFF_NO_PI;
-    strcpy(ifr.ifr_name, "tun0");
+    strncpy(ifr.ifr_name, "tun0", 4);
+
+    // Disable argument validity checking since we are passing ioctl args alcolated in kernel
+    // address space.
+    mm_segment_t fs;
+    fs = get_fs();
+    set_fs(KERNEL_DS);
     long res = vfs_ioctl(file, TUNSETIFF, (u_long)&ifr);
+    set_fs(fs);
     if (res != 0) {
         printk(KERN_INFO "setting tun options failed with code: %ld\n", res);
     }
 
     struct socket* socket = tun_get_socket(file);
     if (IS_ERR_OR_NULL(socket)) {
-        printk(KERN_INFO "fuck!: %lu\n", (long)socket);
+        printk(KERN_INFO "Not nice!: %lu\n", (long)socket);
+    } else {
+        printk(KERN_INFO "Nice!: %lx\n", (long)socket);
     }
+
+    // bool need_copy = false;
+    // int res = dev_ioctl(sock_net(socket->sk), TUNSETIFF, &ifr, &need_copy);
+    // if (res != 0) {
+    //     printk(KERN_INFO "Shit!: %d\n", res);
+    // }
 
     return 0;
 }
